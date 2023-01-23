45d31e40b3a64e1f30bb3253fdb085e2b0c9e548
sarthiraval <78698581+sarthiraval@users.noreply.github.com>
2023-01-23 19:07:43 +0530

    firs

---
+++ React1/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+
+  Text
+} from 'react-native';
+
+
+let App= () =>{
+  return(
+    <Text style={{
+    marginTop : 100,
+    alignSelf :"center"
+        }}>
+      I am Sarthi.
+    </Text>
+  )
+}
+
+export default App;
