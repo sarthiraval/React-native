f215fecb7677b28606eecd667d9365a6565dd651
sarthiraval <78698581+sarthiraval@users.noreply.github.com>
2023-01-25 10:31:39 +0530

    Sa

--- SectionlistandFlatList/App.js
+++ SectionlistandFlatList/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import {
-
+View,FlatList,Text
 } from 'react-native';
 
 let FlasList = () =>{
 
-  
+  return(
+    <View style={{
+      flex :1, 
+      paddingTop: 22
+    }}>
+
+
+   <FlasList data ={[
+      {key : Sarthi},
+      {key : Janik},
+      {key : Sid},
+      {key : Harshit},
+      {key : Kevin},
+      {key : Moin},
+      {key : Keval},
+      {key : Krunal},
+      {key : Yash},
+      {key : Akshat},
+      {key : Deep},
+      {key : Jigar},
+      {key : RamVillas},
+      {key : Komit},
+      {key : Ali},
+   ]}
+   renderItem = {(item) =>
+    <Text style={{padding : 10,fontSize:20,height:44}}>{item.key}</Text>}
+   />
+    </View>
+  )
+
 }
 
 export default FlasList;
