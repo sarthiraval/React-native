5a396627f78ccee6b402d40dff37f02d51653af2
sarthiraval <78698581+sarthiraval@users.noreply.github.com>
2023-01-30 12:07:42 +0530

    Sae

--- FileStructure/src/Screens/register/index.js
+++ FileStructure/src/Screens/register/index.js
@@ -39,27 +39,29 @@ let Reg =()=>{
     const [values, setValues] = useState();
     const [chekdone, setnotdone] = useState (true)
     return(
-        <View style={{flex:1}}>
-            <ImageBackground 
+        <View style={{flex:1,backgroundColor:""}}>
+            {/* <ImageBackground 
             source={{
                 uri: 'https://images.pling.com/img/00/00/62/67/35/1584405/577c4dc30402a8dfa3915867a59e64a89b7b3498e9f327bcfb0bcc7dfcbf99908e32.jpg'    }}
              style ={{flex:1}}
-         >
+         > */}
         <SafeAreaView style={{alignSelf:"center"}}>
             <ScrollView
             showsVerticalScrollIndicator={false}>
               
              <View>
-                <View style={{flexDirection:"row",alignSelf:"center",marginTop:10}}>
+             <Text style={{color:"black",fontSize:25,fontWeight:"bold",alignSelf:"center",marginTop:10}}>Sign Up</Text>
+
+                {/* <View style={{flexDirection:"row",alignSelf:"center",marginTop:10}}>
                 <Text style={{color:"white",fontSize:25,fontWeight:"bold"}}>Sign</Text>
                 <Text style={{color:"yellow",fontSize:25,fontWeight:"bold",left:5}}>Up</Text>
-            </View>
+            </View> */}
             </View>
             <View style={box.rectangle} >
                 <TextInput 
                 placeholder="Your Name"
-                placeholderTextColor={"#999966"}
-                style={{marginTop:5,color:"black",fontSize:15,marginLeft:10,marginTop:10}}/>
+                placeholderTextColor={"#808080"}
+                style={{marginTop:5,color:"black",fontSize:15,marginLeft:10,marginTop:10,}}/>
              </View>    
 
              <View style={box.rectangl} >
@@ -68,7 +70,7 @@ let Reg =()=>{
                 keyboardType= "email-address"
                 dataDetectorTypes="address"
                 textContentType= "emailAddress"
-                placeholderTextColor={"#999966"}
+                placeholderTextColor={"#808080"}
                 style={{marginTop:5,color:"black",fontSize:15,marginLeft:10,marginTop:10}}/>
              </View> 
              <View style={box.rectangl} >
@@ -76,7 +78,7 @@ let Reg =()=>{
                 placeholder="Password"
                 textContentType= "password"
                 secureTextEntry= {true}
-                placeholderTextColor={"#999966"}
+                placeholderTextColor={"#808080"}
                 style={{marginTop:5,color:"black",fontSize:15,marginLeft:10,marginTop:10}}/>
              </View> 
 
@@ -86,20 +88,20 @@ let Reg =()=>{
                 keyboardType= "phone-pad"
                 dataDetectorTypes="phoneNumber"
                 textContentType="telephoneNumber"
-                placeholderTextColor={"#999966"}
+                placeholderTextColor={"#808080"}
                 style={{marginTop:5,color:"black",fontSize:15,marginLeft:10,marginTop:10}}/>
              </View> 
          <View style ={box.rectangl}>
                 <TextInput 
                 placeholder="Street and House Number"
-                placeholderTextColor={"#999966"}
+                placeholderTextColor={"#808080"}
                 style={{marginTop:5,color:"black",fontSize:15,marginLeft:10,marginTop:10}}/>
             </View>
 
             <View style={box.rectangl} >
                 <TextInput 
                 placeholder="Landmark"
-                placeholderTextColor={"#999966"}
+                placeholderTextColor={"#808080"}
                 style={{color:"black",fontSize:15,marginLeft:10,marginTop:10}}/>
              </View>    
    
@@ -109,9 +111,9 @@ let Reg =()=>{
         <View style={box.rectangl} >
              <Dropdown
                
-                placeholderStyle={{marginRight:10,marginLeft:10,fontSize:16,color:"#999966"}}
-                selectedTextStyle={{marginLeft:10,fontSize:16,color:"#999966"}}
-                inputSearchStyle={{marginLeft:10,fontSize:16,color:"#999966"}}
+                placeholderStyle={{marginRight:10,marginLeft:10,fontSize:16,color:"black"}}
+                selectedTextStyle={{marginLeft:10,fontSize:16,color:"black"}}
+                inputSearchStyle={{marginLeft:10,fontSize:16,color:"black"}}
                 data={data}
                 search
                 maxHeight={300}
@@ -129,9 +131,9 @@ let Reg =()=>{
     
           <View style={box.rectangl} >
              <Dropdown
-              placeholderStyle={{marginRight:10,marginLeft:10,fontSize:16,color:"#999966"}}
-                selectedTextStyle={{marginLeft:10,fontSize:16,color:"#999966"}}
-                inputSearchStyle={{marginLeft:10,fontSize:16,color:"#999966"}}
+              placeholderStyle={{marginRight:10,marginLeft:10,fontSize:16,color:"black"}}
+                selectedTextStyle={{marginLeft:10,fontSize:16,color:"black"}}
+                inputSearchStyle={{marginLeft:10,fontSize:16,color:"black"}}
                 data={data}
                 search
                 maxHeight={300}
@@ -149,7 +151,7 @@ let Reg =()=>{
                 <TextInput 
                 placeholder="Pincode"
                 keyboardType= "number-pad"
-                placeholderTextColor={"#999966"}
+                placeholderTextColor={"#808080"}
                 style={{marginTop:5,color:"black",fontSize:15,marginLeft:10,marginTop:10}}/>
              </View> 
         
@@ -178,33 +180,34 @@ let Reg =()=>{
 
 
                 <View>
-                 <Text style={{color:"white",fontSize:10,fontWeight:"bold",left:2}}>I agree to abide by Company's </Text>
-                <Text style={{color:"white",fontSize:10,fontWeight:"bold",left:2,top:3}}>and</Text>
+                 <Text style={{color:"#808080",fontSize:10,fontWeight:"bold",left:2}}>I agree to abide by Company's </Text>
+                <Text style={{color:"#808080",fontSize:10,fontWeight:"bold",left:2,top:3}}>and</Text>
                 </View>
                 <View style={{top:-1}}>
-                <Text style={{color:"yellow",fontSize:12,fontWeight:"bold",left:5}}>Terms of Service</Text>
-                <Text style={{color:"yellow",fontSize:12,fontWeight:"bold",left:-130}}>Its Privacy Policy</Text>
+                <Text style={{color:"blue",fontSize:12,fontWeight:"bold",left:5}}>Terms of Service</Text>
+                <Text style={{color:"blue",fontSize:12,fontWeight:"bold",left:-130}}>Its Privacy Policy</Text>
                 </View>
      </View>
 
  
 
              
-           <View style={{backgroundColor:"#ff80d5",alignSelf:"center",
+           <View style={{backgroundColor:"darkblue",alignSelf:"center",
             marginTop:10,width:330,justifyContent:"center",padding:10,borderRadius:10}}>
               <Text style={{fontSize:20,color:"white",textAlign:"center",fontWeight:"bold"}}> Sign up</Text>
            </View>
              
            <View style={{flexDirection:"row",alignSelf:"center",marginTop:10}}>
-                <Text style={{color:"white",fontSize:12}}>Already have an account?</Text>
-                <Text style={{color:"yellow",fontSize:12,fontWeight:"bold",left:2}}>Click here</Text>
-                <Text style={{color:"white",fontSize:12,left:5}}>to login</Text>
+                <Text style={{color:"#808080",fontSize:12}}>Already have an account?</Text>
+                <Text style={{color:"#00bfff",fontSize:12,fontWeight:"bold",left:2}}>Click here</Text>
+                <Text style={{color:"#808080",fontSize:12,left:5}}>to login</Text>
 
              </View>
       
     </ScrollView>
 </SafeAreaView>
-</ImageBackground>
+{/* </ImageBackground> */}
+
 </View>
     )
 }
@@ -216,6 +219,13 @@ let box = StyleSheet.create({
         marginTop : 40,
         borderRadius: 10,
         backgroundColor: "white",
+        shadowColor: "#999999",
+        shadowOpacity: 0.6,
+        shadowRadius: 1,
+        shadowOffset: {
+        height: 2,
+        }
+        
     },
     rectangl:{
         width: 330,
@@ -223,6 +233,12 @@ let box = StyleSheet.create({
         marginTop : 20,
         borderRadius: 10,
         backgroundColor: "white",
+        shadowColor: "#999999",
+        shadowOpacity: 0.6,
+        shadowRadius: 1,
+        shadowOffset: {
+        height: 2,
+        }
     }}
 )
 export default Reg
