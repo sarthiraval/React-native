22d8c9727f19eba685d86d397f70f6550ad4d767
sarthiraval <78698581+sarthiraval@users.noreply.github.com>
2023-01-31 19:19:08 +0530

    31-1-20222

--- PostInsights/src/Post/index.js
+++ PostInsights/src/Post/index.js
@@ -1,35 +1,82 @@
 import React from 'react'
 import box from './style'
 import { View,Text, SafeAreaView,ScrollView,Image,FlatList} from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
 
 const  data =
 [
-    {id:1,
-     title : 'Sarthi'
+    {id:"58,000",
+     title : 'Likes'
     ,image: 
     {url:
  'https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVyY2VkZXMlMjBjYXJ8ZW58MHx8MHx8&w=1000&q=80'
     }
 },
-    {id:2,title : 'Jainik',
-  image: {url:'https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200'}},
-    {id:3,title : 'Sid',
-  image:{url:"https://www.bugatti.com/fileadmin/_processed_/e/e/csm_og-image_b5397aa5e3.jpg"}},
-    {id:4,title : 'Harshit',
-    image:{url:'https://static.toiimg.com/thumb/msid-90435097,width-1280,height-720,resizemode-4/.jpg'}
+    {id:"2,000",title : 'Likes',
+  image: {url:'https://images.unsplash.com/photo-1620891549027-942fdc95d3f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGVzbGElMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'}},
+    {id:300,title : 'Likes',
+  image:{url:"https://images.unsplash.com/photo-1599114798990-4fac25c3835a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGphZ3VhciUyMGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60"}},
+    {id:"40,000",title : 'Likes',
+    image:{url:'https://images.unsplash.com/photo-1616455164843-dc474e264ba6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGJtdyUyMGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60'}
   },
-    {id:5,title : 'Moin',
-    image:{url:"https://images.news18.com/ibnlive/uploads/2021/07/1627356525_maruti-suzuki-swift-16316273324x3.png"}},
+    {id:"50,000",title : 'Likes',
+    image:{url:"https://images.unsplash.com/photo-1652509373706-32b65f415e33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHZvbHZvJTIwc3BvcnQlMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"}},
 
+ ]
+
+
+ const details = [
+   {
+    title : 'Top Age Ranges',
+    first:"18-24",
+    firstpart:"67%",
+    second:"25-34",
+    secondpart:"43%",
+    thired:"35-44",
+    thiredpart:"23%",
+    fourth:"13-17",
+    fourthpart:"07%"
+  
+},
+  
+{
+  title : 'Top Age Ranges',
+  first:"18-24",
+  firstpart:"67%",
+  second:"25-34",
+  secondpart:"43%",
+  thired:"35-44",
+  thiredpart:"23%",
+  fourth:"13-17",
+  fourthpart:"07%"
+
+},
+{
+  title : 'Top Age Ranges',
+  first:"18-24",
+  firstpart:"67%",
+  second:"25-34",
+  secondpart:"43%",
+  thired:"35-44",
+  thiredpart:"23%",
+  fourth:"13-17",
+  fourthpart:"07%"
+
+}
  ]
 let Post =() =>{
 
     return(
 
 <SafeAreaView style={box.safedesign}>
+{/* <LinearGradient 
+  colors={['#4c669f', '#3b5998', '#192f6a']} 
+  style={box.linearGradient}> */}
 <View style={box.designview}>
-    <Image source={{
-         uri : "https://www.seekpng.com/png/detail/812-8124478_red-left-arrow-icon.png"}} style={box.imagedesign}/>
+    <Image source={
+      require("PostInsights/src/img/left.png")
+      // { uri : "https://www.seekpng.com/png/detail/812-8124478_red-left-arrow-icon.png"}
+         } style={box.imagedesign}/>
    
       <Text style={box.textdesign}>Post Insights</Text>
      </View>
@@ -39,49 +86,91 @@ let Post =() =>{
     showsVerticalScrollIndicator={false}>
     
     
-     <View>
+     <View style={box.flatstyle}>
 
    <FlatList 
     horizontal
     showsHorizontalScrollIndicator ={false}
- 
-      data={data}
+    data={data}
+    style={{marginTop:-10}}
     renderItem={({item}) => 
-    <View>
-          <View style={{
-        borderWidth:12,
-        
-        borderColor:"white",
-        borderRadius:20,
-        width:300,
-        height:300,
-        justifyContent:"center",
-        alignItems:"center"
-
-    }}>
-    <Image 
+   
+   
+    <View style={box.flatlistview}>
+      <Image 
       source={item.image}
-                style={{
-                    height:250,
-                    width:250,
-                    borderRadius:20,
-                    margin:8
-                }}
+                style={box.flatimage}
             />
+     
+      <View style={box.flatviewdesign} >
+        <Text style={box.flattextdesgin}>{item.id}</Text>
+        <Text style={box.flalike}>{item.title}</Text>
         </View>
-        <View style={{flexDirection:"row",alignSelf:"center"}} >
-        <Text style={{ padding: 10, fontSize: 20, height: 50, color: "red" }}>{item.title}</Text>
-    <Text style={{ padding: 10, fontSize: 20, height: 50, color: "red" }}>{item.id}</Text>
-</View>
-            </View>}
+  
+      </View>
+      
+    }
     
     />
      </View>
 
-     <Text style={{ top: 15, fontSize: 20, height: 50, color: "red" ,alignSelf:"center"}}>28,895</Text>
-     <Text style={{ top: 15, fontSize: 20, height: 50, color: "black" ,alignSelf:"center"}}>Total Reach</Text>
+     <Text style={box.normalnumber}>28,895</Text>
+     <Text style={box.heading}>Total Reach</Text>
+
+  <View style={box.boredercolors}/> 
+
+  <Text style={box.headingnew}>Audience Response</Text>
+
+  <FlatList 
+      horizontal
+      showsHorizontalScrollIndicator ={false}
+      data={details}
+      renderItem={({item}) => 
+       <View style={box.flatlistview2}>
+         
+          <View style={box.flatviewdesign2} >
+            <Text style={box.flattextdesgin2}>{item.title}</Text>
+            <Text style={box.flatfirst2}>{item.first}</Text>
+            <View style={box.instyle}>
+            <View style={box.linegrow1} />
+            <View style={box.linegrow2} />
+            <Text style={box.flatfirst3}>{item.firstpart}</Text>
+            </View>
+            
+            
+            <Text style={box.flatfirst2}>{item.second}</Text>
+            <View style={box.instyle}>
+            <View style={box.linegrow3} />
+            <View style={box.linegrow4} />
+            <Text style={box.flatfirst3}>{item.secondpart}</Text>
+            </View>
+
+                
+            <Text style={box.flatfirst2}>{item.thired}</Text>
+            <View style={box.instyle}>
+            <View style={box.linegrow5} />
+            <View style={box.linegrow6} />
+            <Text style={box.flatfirst3}>{item.thiredpart}</Text>
+            </View>
+
+     
+            <Text style={box.flatfirst2}>{item.fourth}</Text>
+            <View style={box.instyle}>
+            <View style={box.linegrow7} />
+            <View style={box.linegrow8} />
+            <Text style={box.flatfirst3}>{item.fourthpart}</Text>
+
+            </View>
+
+            </View>
+      
+      </View>
+      
+    }
+      />
 
 </ScrollView>
+{/* </LinearGradient> */}
 </SafeAreaView>
 
     )}
--- PostInsights/src/Post/style.js
+++ PostInsights/src/Post/style.js
@@ -1,17 +1,122 @@
 import { StyleSheet } from "react-native";
 
 const box = StyleSheet.create({
-         scroll:{flex:1},
+        scroll:{flex:1},
+        linearGradient: {
+            paddingLeft: 15,
+            paddingRight: 15,
+            borderRadius: 5,
+            marginTop: 16,
+            width: 350,
+          },
         safedesign:{
-            backgroundColor: "white",   
-            flex:1
+          backgroundColor:"rgb(248,236,230)",
+              flex:1
         },
         imagedesign:
-            {marginTop:2,height: 15, width: 15 },
-        textdesign:{color:"#FF0000",fontSize:16,alignSelf:"flex-start",marginLeft:8},
-        designview :{flexDirection:"row",alignSelf:"flex-start",marginTop:10,marginLeft:20}
+            {height: 20, width: 20},
+        textdesign:{color:"#FF0000",fontSize:16,alignSelf:"flex-start",marginLeft:5,fontWeight:"500"},
+        designview :{
+            flexDirection:"row",
+            alignSelf:"flex-start",
+            marginTop:10,
+            marginBottom:10,
+            marginLeft:20},
+
+        flatstyle:{ top:30,marginStart:10},
+       
+    flatlistview:{
+        height:280,
+        width:175,
+        borderColor:10,
+        borderWidth:0.5,
+        marginEnd:-5,
+        marginStart:14,
+        borderRadius:10
+    },
+    flatlistview2:{
+        height:340,
+        width:180,
+        backgroundColor:"white"
+
+ 
+    },
+    flatimage:{
+        height:200,
+        top:10,
+        alignSelf:"center",
+        width:160,
+        borderRadius:10,   
+    } ,
+    flatviewdesign:{alignSelf:"center",
+    top:26,bottom:1,flexDirection:"row",borderColor:"red",borderWidth:0.2,borderRadius:11,height:40
+    ,width:160
+    },
+    flatlistview2:{
+         marginTop:35,
+         height:190,
+         width:250,
+         marginStart:10,
+         backgroundColor:"white",
+         borderRadius: 10,
+         paddingStart:20,
+         paddingTop:10,
+         paddingRight:10,
+         marginBottom:80,
+         marginEnd:20
+         
+
+    },
+   
+    flattextdesgin2 :{
+        fontSize: 15, color: "black",fontWeight:"500",fontSize:14,
+        marginBottom:5,left:-7,
+    },
+    flattextdesgin:{
+        fontSize: 15, height: 50, color: "red",marginVertical:10,marginHorizontal:10,fontWeight:"400"},
+    flalike:{fontSize: 15, height: 50, color: "red",marginVertical:10,marginHorizontal:10,left:-15,fontWeight:"400"},
+    normalnumber:{ top: 45, fontSize: 20, height: 50, color: "red" ,alignSelf:"center",fontWeight:"400"},
+    heading :{ top:28, fontSize: 20, height: 50, color: "black" ,alignSelf:"center",
+    fontWeight:"400"},
+    headingnew :{ top:45, fontSize: 20, height: 50, color: "black" ,alignSelf:"center",
+    fontWeight:"400"},
+ 
+    flatfirst2:{fontWeight:"500",fontSize:12, color: "black",marginTop:7
+    ,marginBottom:5,},
+    boredercolors :{
+        borderColor: '#ff9966',borderWidth:0.7,width:360,alignSelf:"center"
+        ,top:20
+    },
+    instyle:{
+       flexDirection:"row"
     },
     
+    linegrow1:{height:5,flexGrow:67,backgroundColor: 'red',
+    borderTopStartRadius:10,borderBottomStartRadius:10},
+    linegrow2:{height:5,flexGrow:33,backgroundColor: '#d9d9d9',
+    borderTopEndRadius:10,borderBottomEndRadius:10
+        },
+
+        linegrow3:{height:5,flexGrow:43,backgroundColor: 'red',
+    borderTopStartRadius:10,borderBottomStartRadius:10},
+    linegrow4:{height:5,flexGrow:57,backgroundColor: '#d9d9d9',
+    borderTopEndRadius:10,borderBottomEndRadius:10
+        },
+
+        linegrow5:{height:5,flexGrow:23,backgroundColor: 'red',
+    borderTopStartRadius:10,borderBottomStartRadius:10},
+    linegrow6:{height:5,flexGrow:77,backgroundColor: '#d9d9d9',
+    borderTopEndRadius:10,borderBottomEndRadius:10
+        },
+        linegrow7:{height:5,flexGrow:7,backgroundColor: 'red',
+        borderTopStartRadius:10,borderBottomStartRadius:10},
+        linegrow8:{height:5,flexGrow:93,backgroundColor: '#d9d9d9',
+        borderTopEndRadius:10,borderBottomEndRadius:10
+            },
+
+        
+    flatfirst3:{marginLeft:8,marginTop:-5,color:"black",fontWeight:"500",fontSize:12,end:3},
+},
     )
 
     export default box
