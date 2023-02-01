ca1773b6b81466a64a5207a37617d9c4d2f49f48
sarthiraval <78698581+sarthiraval@users.noreply.github.com>
2023-02-01 19:10:45 +0530

    1-9ni9bhjpn

--- FileStructure/App.js
+++ FileStructure/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 //import Login from './src/Screens/login'
 // import Reg from './src/Screens/register'
-import Another from './src/Screens/Another'
+// import Another from './src/Screens/Another'
+import YourApp from './src/Screens/Another'
+
 
 
 let App = ()=>{
   return(
    //<Login />
   //  <Reg/>
-   <Another/>
+  //  <Another/>
+  <YourApp />
   )
 }
 
