a7bb962e5cf3b862e14e87f5152468fa4567dd53
sarthiraval <78698581+sarthiraval@users.noreply.github.com>
2023-02-01 14:19:53 +0530

    sarth1-1

--- PostInsights/src/Post/index.js
+++ PostInsights/src/Post/index.js
@@ -1,182 +1,190 @@
 import React from 'react'
-import box from './style'
-import { View,Text, SafeAreaView,ScrollView,Image,FlatList} from 'react-native'
+import styles from './style'
+import { View, Text, SafeAreaView, ScrollView, Image, FlatList } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
 
-const  data =
-[
-    {id:"58,000",
-     title : 'Likes'
-    ,image: 
-    {url:
- 'https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVyY2VkZXMlMjBjYXJ8ZW58MHx8MHx8&w=1000&q=80'
-    }
-},
-    {id:"2,000",title : 'Likes',
-  image: {url:'https://images.unsplash.com/photo-1620891549027-942fdc95d3f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGVzbGElMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60'}},
-    {id:300,title : 'Likes',
-  image:{url:"https://images.unsplash.com/photo-1599114798990-4fac25c3835a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGphZ3VhciUyMGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60"}},
-    {id:"40,000",title : 'Likes',
-    image:{url:'https://images.unsplash.com/photo-1616455164843-dc474e264ba6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGJtdyUyMGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60'}
+const data =
+  [
+    {
+      id: "58,000",
+      title: 'Likes',
+      image:
+        { url: 'https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVyY2VkZXMlMjBjYXJ8ZW58MHx8MHx8&w=1000&q=80' }
+    },
+    {
+      id: "2,000", title: 'Likes',
+      image: { url: 'https://images.unsplash.com/photo-1620891549027-942fdc95d3f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGVzbGElMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60' }
+    },
+    {
+      id: 300, title: 'Likes',
+      image: { url: "https://images.unsplash.com/photo-1599114798990-4fac25c3835a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGphZ3VhciUyMGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60" }
+    },
+    {
+      id: "40,000", title: 'Likes',
+      image: { url: 'https://images.unsplash.com/photo-1616455164843-dc474e264ba6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGJtdyUyMGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60' }
+    },
+    {
+      id: "50,000", title: 'Likes',
+      image: { url: "https://images.unsplash.com/photo-1652509373706-32b65f415e33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHZvbHZvJTIwc3BvcnQlMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60" }
+    },
+    {
+      id: "50,000", title: 'Likes',
+      image: { url: "https://images.unsplash.com/photo-1652509373706-32b65f415e33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHZvbHZvJTIwc3BvcnQlMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60" }
+    },
+
+  ]
+
+
+const details = [
+  {
+    title: 'Top Age Ranges',
+    first: "18-24",
+    firstpart: "67%",
+    second: "25-34",
+    secondpart: "43%",
+    thired: "35-44",
+    thiredpart: "23%",
+    fourth: "13-17",
+    fourthpart: "07%"
+
   },
-    {id:"50,000",title : 'Likes',
-    image:{url:"https://images.unsplash.com/photo-1652509373706-32b65f415e33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHZvbHZvJTIwc3BvcnQlMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"}},
-    {id:"50,000",title : 'Likes',
-  image:{url:"https://images.unsplash.com/photo-1652509373706-32b65f415e33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHZvbHZvJTIwc3BvcnQlMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"}},
 
- ]
+  {
+    title: 'Top Age Ranges',
+    first: "18-24",
+    firstpart: "67%",
+    second: "25-34",
+    secondpart: "43%",
+    thired: "35-44",
+    thiredpart: "23%",
+    fourth: "13-17",
+    fourthpart: "07%"
 
+  },
+  {
+    title: 'Top Age Ranges',
+    first: "18-24",
+    firstpart: "67%",
+    second: "25-34",
+    secondpart: "43%",
+    thired: "35-44",
+    thiredpart: "23%",
+    fourth: "13-17",
+    fourthpart: "07%"
 
- const details = [
-   {
-    title : 'Top Age Ranges',
-    first:"18-24",
-    firstpart:"67%",
-    second:"25-34",
-    secondpart:"43%",
-    thired:"35-44",
-    thiredpart:"23%",
-    fourth:"13-17",
-    fourthpart:"07%"
-  
-},
-  
-{
-  title : 'Top Age Ranges',
-  first:"18-24",
-  firstpart:"67%",
-  second:"25-34",
-  secondpart:"43%",
-  thired:"35-44",
-  thiredpart:"23%",
-  fourth:"13-17",
-  fourthpart:"07%"
+  }
+]
 
-},
-{
-  title : 'Top Age Ranges',
-  first:"18-24",
-  firstpart:"67%",
-  second:"25-34",
-  secondpart:"43%",
-  thired:"35-44",
-  thiredpart:"23%",
-  fourth:"13-17",
-  fourthpart:"07%"
+let Post = () => {
 
-}
- ]
-let Post =() =>{
+  return (
 
-    return(
+    <SafeAreaView style={styles.safedesign}>
 
-<SafeAreaView style={box.safedesign}>
-{/* <LinearGradient 
-  colors={['#4c669f', '#3b5998', '#192f6a']} 
-  style={box.linearGradient}> */}
-<View style={box.designview}>
-    <Image source={
-      require("PostInsights/src/img/left.png")
-      // { uri : "https://www.seekpng.com/png/detail/812-8124478_red-left-arrow-icon.png"}
-         } style={box.imagedesign}/>
-   
-      <Text style={box.textdesign}>Post Insights</Text>
-     </View>
+      <View style={styles.designview}>
+        <Image source={
+          require("PostInsights/src/img/left.png")
+          // { uri : "https://www.seekpng.com/png/detail/812-8124478_red-left-arrow-icon.png"}
+        } style={styles.imagedesign} />
 
-    <ScrollView
-    style={box.scroll} 
-    showsVerticalScrollIndicator={false}>
-    
-    
-     <View style={box.flatstyle}>
-
-   <FlatList 
-    horizontal
-    style={{margin:2}}
-    showsHorizontalScrollIndicator ={false}
-    data={data}
-    pagingEnabled={true}
-
-    
-    renderItem={({item}) => 
-    
-    <View style={box.flatlistview}>
-      <Image 
-      source={item.image}
-                style={box.flatimage}
-            />
-     
-      <View style={box.flatviewdesign} >
-        <Text style={box.flattextdesgin}>{item.id}</Text>
-        <Text style={box.flalike}>{item.title}</Text>
-        </View>
-  
+        <Text style={styles.textdesign}>Post Insights</Text>
       </View>
-      
-    }
-    
-    />
-     </View>
 
-     <Text style={box.normalnumber}>28,895</Text>
-     <Text style={box.heading}>Total Reach</Text>
+      <ScrollView
+        style={styles.scroll}
+        showsVerticalScrollIndicator={false}>
 
-  <View style={box.boredercolors}/> 
 
-  <Text style={box.headingnew}>Audience Response</Text>
+        <View style={styles.flatstyle}>
 
-  <FlatList 
-      horizontal
-      showsHorizontalScrollIndicator ={false}
-      data={details}
-      renderItem={({item}) => 
+          <FlatList
+            horizontal
+            style={{ margin: 2 }}
+            showsHorizontalScrollIndicator={false}
+            data={data}
+            pagingEnabled={true}
 
-       <View style={box.flatlistview2}>
-         
-      
-            <Text style={box.flattextdesgin2}>{item.title}</Text>
-            <Text style={box.flatfirst2}>{item.first}</Text>
-            <View style={box.instyle}>
-            <View style={box.linegrow1} />
-            <View style={box.linegrow2} />
-            <Text style={box.flatfirst3}>{item.firstpart}</Text>
+
+            renderItem={({ item }) =>
+
+              <View style={styles.flatlistview}>
+                <Image
+                  source={item.image}
+                  style={styles.flatimage}
+                />
+
+                <View style={styles.flatviewdesign} >
+                  <Text style={styles.flattextdesgin}>{item.id}</Text>
+                  <Text style={styles.flalike}>{item.title}</Text>
+                </View>
+
+              </View>
+
+            }
+
+          />
+        </View>
+
+        <Text style={styles.normalnumber}>28,895</Text>
+        <Text style={styles.heading}>Total Reach</Text>
+
+        <View style={styles.boredercolors} />
+
+        <Text style={styles.headingnew}>Audience Response</Text>
+
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={details}
+          renderItem={({ item }) =>
+
+            <View style={styles.flatlistview2}>
+
+
+              <Text style={styles.flattextdesgin2}>{item.title}</Text>
+              <Text style={styles.flatfirst2}>{item.first}</Text>
+              <View style={styles.instyle}>
+                <View style={styles.linegrow1} />
+                <View style={styles.linegrow2} />
+                <Text style={styles.flatfirst3}>{item.firstpart}</Text>
+              </View>
+
+
+              <Text style={styles.flatfirst2}>{item.second}</Text>
+              <View style={styles.instyle}>
+                <View style={styles.linegrow3} />
+                <View style={styles.linegrow4} />
+                <Text style={styles.flatfirst3}>{item.secondpart}</Text>
+              </View>
+
+
+              <Text style={styles.flatfirst2}>{item.thired}</Text>
+              <View style={styles.instyle}>
+                <View style={styles.linegrow5} />
+                <View style={styles.linegrow6} />
+                <Text style={styles.flatfirst3}>{item.thiredpart}</Text>
+              </View>
+
+
+              <Text style={styles.flatfirst2}>{item.fourth}</Text>
+              <View style={styles.instyle}>
+                <View style={styles.linegrow7} />
+                <View style={styles.linegrow8} />
+                <Text style={styles.flatfirst3}>{item.fourthpart}</Text>
+
+              </View>
+
             </View>
-            
-            
-            <Text style={box.flatfirst2}>{item.second}</Text>
-            <View style={box.instyle}>
-            <View style={box.linegrow3} />
-            <View style={box.linegrow4} />
-            <Text style={box.flatfirst3}>{item.secondpart}</Text>
-            </View>
 
-                
-            <Text style={box.flatfirst2}>{item.thired}</Text>
-            <View style={box.instyle}>
-            <View style={box.linegrow5} />
-            <View style={box.linegrow6} />
-            <Text style={box.flatfirst3}>{item.thiredpart}</Text>
-            </View>
 
-     
-            <Text style={box.flatfirst2}>{item.fourth}</Text>
-            <View style={box.instyle}>
-            <View style={box.linegrow7} />
-            <View style={box.linegrow8} />
-            <Text style={box.flatfirst3}>{item.fourthpart}</Text>
+          }
+        />
 
-            </View>
+      </ScrollView>
+      {/* </LinearGradient> */}
+    </SafeAreaView>
 
-            </View>
-         
-      
-    }
-      />
+  )
+}
 
-</ScrollView>
-{/* </LinearGradient> */}
-</SafeAreaView>
-
-    )}
-
- export default   Post                     
+export default Post                     
--- PostInsights/src/Post/style.js
+++ PostInsights/src/Post/style.js
@@ -1,175 +1,235 @@
 import { StyleSheet } from "react-native";
 
-const box = StyleSheet.create({
-        scroll:{flex:1},
-        linearGradient: {
-            paddingLeft: 15,
-            paddingRight: 15,
-            borderRadius: 5,
-            marginTop: 16,
-            width: 350,
-          },
-        safedesign:{
-          backgroundColor:"rgb(248,236,230)",
-              flex:1
-        },
-        imagedesign:
-            {height: 20, width: 20},
-        textdesign:{
-            color:"#FF0000",
-            fontSize:16,
-            alignSelf:"flex-start",
-            marginLeft:5,
-            fontWeight:"400"},
-        designview :{
-            flexDirection:"row",
-            alignSelf:"flex-start",
-            marginBottom:10,
-            marginLeft:20},
+const styles = StyleSheet.create({
+    scroll: { flex: 1 },
+    linearGradient: {
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 5,
+        marginTop: 16,
+        width: 350,
+    },
+    safedesign: {
+        backgroundColor: "rgb(248,236,230)",
+        flex: 1
+    },
+    imagedesign:
+        { height: 20, width: 20 },
+    textdesign: {
+        color: "#FF0000",
+        fontSize: 16,
+        alignSelf: "flex-start",
+        marginLeft: 5,
+        fontWeight: "400"
+    },
+    designview: {
+        flexDirection: "row",
+        alignSelf: "flex-start",
+        marginBottom: 10,
+        marginLeft: 20
+    },
 
-    flatstyle:{top:20,width:350,alignSelf:"center"},
-       
-    flatlistview:{
-        height:298,
-        alignItems:"center",
-        width:170,
-        borderColor:10,
-        flex:0.5,
+    flatstyle: {
+        top: 20,
+        width: 350,
+        alignSelf: "center"
+    },
+
+    flatlistview: {
+        height: 298,
+        alignItems: "center",
+        width: 170,
+        borderColor: 10,
+        flex: 0.5,
         marginBottom: 5,
-        borderWidth:0.5,
-        borderRadius:10,
-        
-        marginStart:5,
+        borderWidth: 0.5,
+        borderRadius: 10,
+
+        marginStart: 5,
     },
- 
-    flatimage:{
-        height:215,
-        top:10,
-        alignSelf:"center",
-        width:148,
-        borderRadius:10,   
-    } ,
-    flatviewdesign:{
-        alignSelf:"center",
-        top:31,
-        bottom:1,
-        flexDirection:"row",
-        borderColor:"#ffcccc",
-        borderWidth:1,
-        borderRadius:11,
-        height:40,
-        backgroundColor:"rgb(250,239,232)",
-        width:140
+
+    flatimage: {
+        height: 215,
+        top: 10,
+        alignSelf: "center",
+        width: 148,
+        borderRadius: 10,
     },
-    flatlistview2:{
-         marginTop:30,
-         height:205,
-         width:260,
-         marginStart:10,
-         backgroundColor:"white",
-         borderRadius: 10,
-         paddingStart:20,
-         paddingTop:15,
-         borderColor:"grey",
-         borderWidth:0.2,
-         paddingRight:10,
-         marginBottom:80,
-         marginEnd:15,
-         
-        
+    flatviewdesign: {
+        alignSelf: "center",
+        top: 31,
+        bottom: 1,
+        flexDirection: "row",
+        borderColor: "#ffcccc",
+        borderWidth: 1,
+        borderRadius: 11,
+        height: 40,
+        backgroundColor: "rgb(250,239,232)",
+        width: 140
+    },
+    flatlistview2: {
+        marginTop: 30,
+        height: 205,
+        width: 260,
+        marginStart: 10,
+        backgroundColor: "white",
+        borderRadius: 10,
+        paddingStart: 20,
+        paddingTop: 15,
+        borderColor: "grey",
+        borderWidth: 0.2,
+        paddingRight: 10,
+        marginBottom: 80,
+        marginEnd: 15,
+
+
 
     },
-   
-   
-    flattextdesgin:{
-        fontSize: 15, height: 50, color: "red",marginVertical:10,
-        marginHorizontal:10,fontWeight:"500",fontSize:14,
-    shadowColor:"red"},
-    flalike:{fontWeight:"500",fontSize:14, height: 50, color: "red",marginVertical:10,marginHorizontal:10,left:-15,fontWeight:"400"},
-    normalnumber:{ 
-        top: 35, 
+
+
+    flattextdesgin: {
+        fontSize: 15,
+        height: 50,
+        color: "red",
+        marginVertical: 10,
+        marginHorizontal: 10,
+        fontWeight: "500",
+        fontSize: 14,
+        shadowColor: "red"
+    },
+    flalike: {
+        fontWeight: "500",
+        fontSize: 14,
+        height: 50,
+        color: "red",
+        marginVertical: 10,
+        marginHorizontal: 10,
+        left: -15,
+        fontWeight: "400"
+    },
+    normalnumber: {
+        top: 35,
         fontSize: 20,
-         height: 50, 
-         color: "red" 
-         ,alignSelf:"center"
-         ,fontWeight:"400"
-        },
-        
-    boredercolors :{
+        height: 50,
+        color: "red"
+        , alignSelf: "center"
+        , fontWeight: "400"
+    },
+
+    boredercolors: {
         borderColor: '#ff9966',
-        borderWidth:0.7,
-        width:360,
-        alignSelf:"center"
-        ,top:13
+        borderWidth: 0.7,
+        width: 360,
+        alignSelf: "center"
+        , top: 13
     },
-    heading :{ 
-        top:16, 
-        fontSize: 20, 
-        height: 50, 
+    heading: {
+        top: 16,
+        fontSize: 20,
+        height: 50,
         color: "black"
-         ,alignSelf:"center",
-    fontWeight:"400"},
-    headingnew :{
-         top:45, 
-         fontSize: 20, 
-         height: 50,
-          color: "black" ,
-          alignSelf:"center",
-        fontWeight:"400",
-        marginBottom:10},
- 
-    flatfirst2:{
-        fontWeight:"500",
-        fontSize:11, 
-        color: "black"
-        ,marginTop:11
-        ,marginBottom:5,},
-     
- flatfirst3:{
-            marginLeft:20,
-            marginTop:-5,
-            color:"black",
-            fontWeight:"400",
-            fontSize:12,
-            end:3},
-
-    flattextdesgin2 :{
-                 color: "black"
-                 ,fontWeight:"400",
-                 fontSize:14,
-                 marginTop:-1,
-                marginBottom:3,left:-7,
-            },
-    instyle:{
-       flexDirection:"row"
+        , alignSelf: "center",
+        fontWeight: "400"
     },
-    
-    linegrow1:{height:5,flexGrow:67,backgroundColor: 'red',
-    borderTopStartRadius:10,borderBottomStartRadius:10},
-    linegrow2:{height:5,flexGrow:33,backgroundColor: '#d9d9d9',
-    borderTopEndRadius:10,borderBottomEndRadius:10
-        },
+    headingnew: {
+        top: 45,
+        fontSize: 20,
+        height: 50,
+        color: "black",
+        alignSelf: "center",
+        fontWeight: "400",
+        marginBottom: 10
+    },
 
-        linegrow3:{height:5,flexGrow:43,backgroundColor: 'red',
-    borderTopStartRadius:10,borderBottomStartRadius:10},
-    linegrow4:{height:5,flexGrow:57,backgroundColor: '#d9d9d9',
-    borderTopEndRadius:10,borderBottomEndRadius:10
-        },
+    flatfirst2: {
+        fontWeight: "500",
+        fontSize: 11,
+        color: "black"
+        , marginTop: 11
+        , marginBottom: 5,
+    },
 
-        linegrow5:{height:5,flexGrow:23,backgroundColor: 'red',
-    borderTopStartRadius:10,borderBottomStartRadius:10},
-    linegrow6:{height:5,flexGrow:77,backgroundColor: '#d9d9d9',
-    borderTopEndRadius:10,borderBottomEndRadius:10
-        },
-        linegrow7:{height:5,flexGrow:7,backgroundColor: 'red',
-        borderTopStartRadius:10,borderBottomStartRadius:10},
-        linegrow8:{height:5,flexGrow:93,backgroundColor: '#d9d9d9',
-        borderTopEndRadius:10,borderBottomEndRadius:10
-            },
+    flatfirst3: {
+        marginLeft: 20,
+        marginTop: -5,
+        color: "black",
+        fontWeight: "400",
+        fontSize: 12,
+        end: 3
+    },
 
-        
-        },
-    )
+    flattextdesgin2: {
+        color: "black"
+        , fontWeight: "400",
+        fontSize: 14,
+        marginTop: -1,
+        marginBottom: 3, left: -7,
+    },
+    instyle: {
+        flexDirection: "row"
+    },
 
-    export default box
-    
+    linegrow1: {
+        height: 5,
+        flexGrow: 67,
+        backgroundColor: 'red',
+        borderTopStartRadius: 10,
+        borderBottomStartRadius: 10
+    },
+    linegrow2: {
+        height: 5,
+        flexGrow: 33,
+        backgroundColor: '#d9d9d9',
+        borderTopEndRadius: 10,
+        borderBottomEndRadius: 10
+    },
+
+    linegrow3: {
+        height: 5,
+        flexGrow: 43,
+        backgroundColor: 'red',
+        borderTopStartRadius: 10,
+        borderBottomStartRadius: 10
+    },
+    linegrow4: {
+        height: 5,
+        flexGrow: 57,
+        backgroundColor: '#d9d9d9',
+        borderTopEndRadius: 10,
+        borderBottomEndRadius: 10
+    },
+
+    linegrow5: {
+        height: 5,
+        flexGrow: 23,
+        backgroundColor: 'red',
+        borderTopStartRadius: 10,
+        borderBottomStartRadius: 10
+    },
+    linegrow6: {
+        height: 5,
+        flexGrow: 77,
+        backgroundColor: '#d9d9d9',
+        borderTopEndRadius: 10,
+        borderBottomEndRadius: 10
+    },
+    linegrow7: {
+        height: 5,
+        flexGrow: 7,
+        backgroundColor: 'red',
+        borderTopStartRadius: 10,
+        borderBottomStartRadius: 10
+    },
+    linegrow8: {
+        height: 5,
+        flexGrow: 93,
+        backgroundColor: '#d9d9d9',
+        borderTopEndRadius: 10,
+        borderBottomEndRadius: 10
+    },
+
+
+},
+)
+
+export default styles
