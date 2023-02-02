065fb53121afcf7274c71e74ef1d5687fc605611
sarthiraval <78698581+sarthiraval@users.noreply.github.com>
2023-02-02 11:29:04 +0530

    w

--- FileStructure/App.js
+++ FileStructure/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 //import Login from './src/Screens/login'
 // import Reg from './src/Screens/register'
 // import Another from './src/Screens/Another'
-// import YourApp from './src/Screens/Another/myinfo'
-//  import YourApp from './src/Screens/Another/flex1'
- import YourApp from './src/Screens/Another/flatandsection'
+ import YourApp from './src/Screens/Another/flex1'
+//  import YourApp from './src/Screens/Another/flatandsection'
 
 
 
