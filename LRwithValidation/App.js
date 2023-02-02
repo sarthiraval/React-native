c1afcb86b3f342617520b6602786ff6d28076d8e
sarthiraval <78698581+sarthiraval@users.noreply.github.com>
2023-02-02 14:04:20 +0530

    new project

---
+++ LRwithValidation/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Login from './src/Screens/login'
+
+const App = ()=>{
+  return(
+    <View></View>
+  )
+}
+
+export default App;
---
+++ LRwithValidation/src/screens/login/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native/types';
+
+let Login = ()=>{
+    return(
+        <View></View>
+    )
+}
+
+export default Login
